2702bd6f0494919b97bbac959bb7493e49e9445e
javrin <jawabiscuit@users.noreply.github.com>
2024-02-14 09:50:19 -0500

    Generate project with cookiecutter

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 0.1.0
+
+### Added or Changed
+
+- Generate project with cookiecutter
---
+++ src/bst_learner/__init__.py
@@ -0,0 +1,6 @@
+"""An interactive visualization of binary search trees."""
+from dotenv import load_dotenv
+
+__version__ = "0.1.0"
+
+load_dotenv()
